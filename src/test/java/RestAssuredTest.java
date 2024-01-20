2d889a5552eac7cdd82af2c094630824cbdd6830
Igor0763 <Igor763763763@yandex.ru>
2024-01-20 21:04:30 +0400

    first commit

---
+++ src/test/java/RestAssuredTest.java
@@ -0,0 +1,108 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class RestAssuredTest {
+
+    @Test
+    void checkBodyListUser() {
+        get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .body("page", is(2))
+                .body("per_page", is(6))
+                .body("total", is(12))
+                .body("total_pages", is(2))
+                .body("data.id", hasItems(7, 8, 9))
+                .body("data.first_name", hasItems("Michael", "Lindsay", "Tobias"))
+                .statusCode(200);
+
+    }
+
+    @Test
+    void successfulRegister() {
+        String credentials = "{\n" +
+                "    \"email\": \"eve.holt@reqres.in\",\n" +
+                "    \"password\": \"pistol\"\n" +
+                "}";
+
+        given()
+                .body(credentials)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void missingEmail() {
+        String credentials = "{}";
+
+        given()
+                .body(credentials)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+        ;
+    }
+
+    @Test
+    void missingPassword() {
+        String credentials = "{\"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .body(credentials)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+        ;
+    }
+
+    @Test
+    void missingRegister() {
+        String credentials = "{\"password\": \"sadds\"}";
+
+        given()
+                .body(credentials)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+        ;
+    }
+
+}
+
